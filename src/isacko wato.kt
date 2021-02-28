f7a6206055901bd0d35854f46af04e90d80a2350
Watotacho <watotacho@gmail.com>
2021-02-28 13:06:54 +0300

    kotlin assignment

---
+++ src/isacko wato.kt
@@ -0,0 +1,33 @@
+fun  main() {
+    name()
+   var rem = modulus(27,5)
+    println(rem)
+    var result=addition(10,20,30,40)
+    println (result)
+    fact()
+}
+fun name(){
+    var name= ("Hello wato")
+    println(name)
+
+
+
+}
+
+fun modulus(num1:Int,num2:Int):Int{
+    var modulus=num1%num2
+    return modulus
+}
+fun addition(a:Int,b:Int,c:Int,d:Int):Int{
+    var add=(a+b+c+d)
+    return add
+
+
+
+
+}
+fun fact(){
+    var fact="love coding"
+    print(fact)
+
+}
